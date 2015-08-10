a1bb5afde6efc5fba82804fdd611ccdcac11ae1d
Administrateur <Administrateur@J3ASERVER.Home>
2015-08-10 12:40:34 +0200

    s

--- SherpaCourtage/src/com/j3a/assurance/managedBean/adminCompagnie/ManagedCompagnie.java
+++ SherpaCourtage/src/com/j3a/assurance/managedBean/adminCompagnie/ManagedCompagnie.java
@@ -2,9 +2,14 @@ package com.j3a.assurance.managedBean.adminCompagnie;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.j3a.assurance.model.CompagnieAssurance;
+import com.j3a.assurance.objetService.ObjectService;
 
 
 @Component
@@ -14,7 +19,63 @@ public class ManagedCompagnie implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
+	public ObjectService getObjectService() {
+		return objectService;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public void setObjectService(ObjectService objectService) {
+		this.objectService = objectService;
+	}
 	private CompagnieAssurance compagnieAssurance;
+	@Autowired
+	private ObjectService objectService;
+	
+	
+	
+	public void ajouterCompagnie(){
+		
+	try {
+		getObjectService().addObject(getCompagnieAssurance());
+	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Enregistrement éffectué avec succès!"));
+	} catch (Exception e) {
+				e.printStackTrace();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Enregistrement non éffectué!"));
+	}	
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public CompagnieAssurance getCompagnieAssurance() {
 		return compagnieAssurance;
 	}
